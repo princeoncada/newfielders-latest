2e51e25c9fd2a4696da647a22692c689bef7477a
prince <prince.oncada@gmail.com>
2025-03-19 10:47:25 -0400

    index file of home page component

---
+++ components/Home/index.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import Hero from './Hero'
+import Programs from './Programs'
+import Content from './Content'
+import Founder from './Founder'
+import Gallery from './Gallery'
+import News from './News'
+import CTA from '../partials/CTA'
+
+const index = () => {
+  return (
+    <>
+      <Hero />
+      <Programs />
+      <Content />
+      <Founder />
+      <Gallery />
+      <News />
+      <CTA />
+    </>
+  )
+}
+
+export default index
