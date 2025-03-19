594ea32a3d9447cc7ef37f5ecabb92fed47426c5
prince <prince.oncada@gmail.com>
2025-03-19 10:47:57 -0400

    created CTA partials component

---
+++ components/partials/CTA.tsx
@@ -0,0 +1,25 @@
+import Link from 'next/link'
+import React from 'react'
+
+const CTA = () => {
+  return (
+    <section
+      className="h-64 xl:h-[420px] text-white font-heading px-24 flex flex-col justify-center items-center gap-5 xl:gap-8 text-center"
+      style={{
+        backgroundImage:
+          "linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url('/images/DSC_7818.png')",
+        backgroundSize: "120%",
+        backgroundPosition: "50% 15%",
+      }}
+    >
+      <h1 className="text-2xl font-bold xl:text-[48px]">
+        Kickstart your child&apos;s journey
+      </h1>
+      <Link href="/admission" className="bg-[#D2A808] text-lg font-medium xl:py-[14px] xl:px-[40px] py-2 px-4 xl:text-[24px]">
+        Join us
+      </Link>
+    </section>
+  )
+}
+
+export default CTA
