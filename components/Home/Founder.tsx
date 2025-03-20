24af95ea5a44a956fad1f9953f2a2e1b8dd93f92
prince <prince.oncada@gmail.com>
2025-03-20 15:50:52 -0400

    added px-4 to home founder section so that there's a little padding when in 1440px screen width

--- components/Home/Founder.tsx
+++ components/Home/Founder.tsx
@@ -3,7 +3,7 @@ import { HOME_CONTENTS } from "@/lib/constants"
 const Founder = () => {
   return (
     <section>
-      <div className="py-8 xl:py-12 flex justify-center max-w-[1440px] mx-auto">
+      <div className="py-8 xl:py-12 flex justify-center max-w-[1440px] mx-auto px-4">
         <div className="flex items-center w-full aspect-[1/1.2] md:aspect-video bg-cover bg-[center_top_-1.5rem] md:bg-[top_-6rem_left_12rem]" style={{ backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(${HOME_CONTENTS.founder.image.src})` }}>
           <div className="space-y-4 text-white px-4 md:px-16">
             <p className="text-center md:w-1/2 mx-auto italic font-semibold md:text-start md:mx-0 md:text-[20px] xl:text-[28px]">
