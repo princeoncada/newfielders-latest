1df057b0652aea83f4492809a165a25ff061141b
prince <prince.oncada@gmail.com>
2025-03-20 15:51:21 -0400

    adjusted to use px-4 in home gallery section so that there's a little padding when in 1440px screen width

--- components/Home/Gallery.tsx
+++ components/Home/Gallery.tsx
@@ -6,11 +6,11 @@ import { HOME_CONTENTS } from "@/lib/constants"
 const Gallery = () => {
   return (
     <section>
-      <div className="max-w-[1440px] mx-auto px-4 py-8 xl:px-0 xl:py-12">
+      <div className="max-w-[1440px] mx-auto px-4 py-8 xl:py-12">
         <h1 className="text-2xl font-bold mb-2 text-[#028141] xl:text-[48px] xl:mb-5">
           {HOME_CONTENTS.gallery.title}
         </h1>
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-2 md:gap-y-4">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-2 md:gap-4">
           {
             HOME_CONTENTS.gallery.content.map((gallery, index) => (
               <Link key={`${HOME_CONTENTS.gallery.title}-${gallery.title}-${index}`} href={gallery.link}>
@@ -20,7 +20,7 @@ const Gallery = () => {
                 <Image
                   className="rounded-md shadow-lg"
                   src={gallery.image}
-                  alt=""
+                  alt={`${gallery.title}-image`}
                   width="1000"
                   height="800"
                 ></Image>
